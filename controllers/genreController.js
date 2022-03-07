43408537012b7365df27a9d46f1be4ed5ac3f2fa
ikari-shirei <algrsyu@protonmail.com>
2022-03-07 04:45:13 +0300

    Add genre update get / post

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -175,11 +175,64 @@ exports.genre_delete_post = function (req, res, next) {
 }
 
 // Display Genre update form on GET.
-exports.genre_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre update GET')
+exports.genre_update_get = function (req, res, next) {
+  Genre.find({ _id: req.params.id }).exec(function (err, result) {
+    if (err) {
+      return next(err)
+    }
+
+    // Successful
+    res.render('genre_form', {
+      title: 'Update genre',
+      genre: result[0],
+    })
+  })
 }
 
 // Handle Genre update on POST.
-exports.genre_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre update POST')
-}
+exports.genre_update_post = [
+  // Validate
+  body('name', 'Genre must not be empty').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req)
+
+    var genre = new Genre({ name: req.body.name, _id: req.params.id })
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre: genre,
+        errors: errors.array(),
+      })
+      return
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ name: req.body.name }).exec(function (err, found_genre) {
+        if (err) {
+          return next(err)
+        }
+
+        if (found_genre) {
+          // Genre exists, redirect to its detail page.
+          res.redirect(found_genre.url)
+        } else {
+          Genre.findByIdAndUpdate(
+            req.params.id,
+            genre,
+            {},
+            function (err, thegenre) {
+              if (err) {
+                return next(err)
+              }
+              // Genre saved. Redirect to genre detail page.
+              res.redirect(thegenre.url)
+            }
+          )
+        }
+      })
+    }
+  },
+]
